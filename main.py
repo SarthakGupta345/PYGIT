5ffb980027ea1fcb6b0d58f55aa97a5b0e32307e
SarthakGupta345 <Sarthak987gupta654@gmail.com>
2026-05-25 13:57:10 +0530

    Done some changes

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import argparse
+import sys
 print("Hello world")
 
 def main():
     parser = argparse.ArgumentParser(
-        description='Pygit to track version'
+        description='Pygit A version controller to track version'
     )
     subparser = parser.add_subparsers(
         dest='Command' , help='Available commands'
@@ -11,8 +12,22 @@ def main():
 
     init_parser = subparser.add_parser("init", help='Initialize a new repository')
     check_parser = subparser.add_parser("check", help='checking  a new repository')
+    add_parser =     check_parser = subparser.add_parser("add", help='add  a new repository')
+
 
     args = parser.parse_args()
     print(args)
+    
+    if not args.Command:
+        parser.print_help()
+        return 
+    
+    try:
+        if args.Command =='init':
+            pass
+    except Exception as e:
+        print(f'Error: {e}')
+        sys.exit(1)
+
 
 main()
