8c9942b027a7e84a0fb649fd9c9261008686a1e8
SarthakGupta345 <Sarthak987gupta654@gmail.com>
2026-05-25 13:51:32 +0530

    parser adddeed

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+import argparse
 print("Hello world")
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='Pygit to track version'
+    )
+    subparser = parser.add_subparsers(
+        dest='Command' , help='Available commands'
+    )
+
+    init_parser = subparser.add_parser("init", help='Initialize a new repository')
+    check_parser = subparser.add_parser("check", help='checking  a new repository')
+
+    args = parser.parse_args()
+    print(args)
+
+main()
