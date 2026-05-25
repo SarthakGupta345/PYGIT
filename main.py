8678e2f3f145857578a4ee3fd048063d9e4158be
SarthakGupta345 <Sarthak987gupta654@gmail.com>
2026-05-25 14:07:28 +0530

    class directories are done

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
 import argparse
+from pathlib import Path
 import sys
 print("Hello world")
+
+class Repository:
+    def __init__(self,path='.') -> None:
+        self.path = Path(path).resolve()
+        self.git_dir = self.path/".pygit"
+
+        self.object_dir = self.git_dir/'objects'
+        self.ref_dir = self.git_dir/'refs'
+        self.head_dir = self.git_dir/'HEAD'
+
+        self.index_file = self.git_dir/'index'
+
+    def init(self)-> bool:
+        # directories
+        self.git_dir.mkdir()
+        self.object_dir.mkdir()
+        self.ref_dir.mkdir()
+
+        return True
+
 
 def main():
     parser = argparse.ArgumentParser(
